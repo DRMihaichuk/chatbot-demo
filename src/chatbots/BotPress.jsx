87138b851bff34196f0a0aada621fd1aebd057f0
DRMihaichuk <drmihaichuk@wpi.edu>
2025-04-08 00:51:13 +0300

    Initial commit

---
+++ src/chatbots/BotPress.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BotPress = () => {
+    return (
+        <>
+            <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
+            <script src="https://files.bpcontent.cloud/2025/03/26/13/20250326131616-6VYTAGEN.js"></script>
+        </>
+    );
+};
+
+export default BotPress;
